db111d1d6de56096002c45a528825209d87dc4aa
grahsp <pontus.grahsp@gmail.com>
2025-05-17 18:14:09 +0200

    feat: add method to update state and transition - add OnUpdate method to states

--- StateMachine/StateMachine.cs
+++ StateMachine/StateMachine.cs
@@ -8,36 +8,47 @@ public class StateMachine<TContext> where TContext : class
 	{
 		CurrentState = initialState;
 	}
-}
 
-public class Transition<TContext> where TContext : class
-{
-	public State<TContext> Source { get; }
-	public State<TContext> Target { get; }
-	public Func<TContext, bool> Condition { get; }
-	
-
-	public Transition(State<TContext> source, State<TContext> target, Func<TContext, bool> condition)
+	public void Update(TContext context)
 	{
-		Source = source;
-		Target = target;
-		Condition = condition;
+		CurrentState.OnUpdate(context);
+		
+		var transition = CurrentState.Transitions
+			.FirstOrDefault(t => t.Condition(context));
+
+		if (transition is null)
+			return;
+		
+		CurrentState.OnExit(context);
+		CurrentState = transition.Target;
+		CurrentState.OnEnter(context);
 	}
 }
 
-public abstract class State<TContext> : IState where TContext : class
+public class Transition<TContext>
+	(State<TContext> source, State<TContext> target, Func<TContext, bool> condition)
+	where TContext : class
+{
+	public State<TContext> Source { get; } = source;
+	public State<TContext> Target { get; } = target;
+	public Func<TContext, bool> Condition { get; } = condition;
+}
+
+public abstract class State<TContext> : IState<TContext> where TContext : class
 {
 	internal readonly List<Transition<TContext>> Transitions = [];
 
 	internal void AddTransition(State<TContext> target, Func<TContext, bool> condition)
 		=> Transitions.Add(new Transition<TContext>(this, target, condition));
 
-	public abstract void OnEnter();
-	public abstract void OnExit();
+	public virtual void OnEnter(TContext context) { }
+	public virtual void OnExit(TContext context) { }
+	public virtual void OnUpdate(TContext context) { }
 }
 
-public interface IState
+public interface IState<in TContext> where TContext : class
 {
-	void OnEnter();
-	void OnExit();
+	void OnEnter(TContext context);
+	void OnExit(TContext context);
+	void OnUpdate(TContext context);
 }
